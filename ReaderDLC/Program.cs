66c82e8dc01736ef48821222b37e029156d0a848
x <f6runner@gmail.com>
2017-07-08 13:50:52 +0100

    New source , rebuilt

--- ReaderDLC/Program.cs
+++ ReaderDLC/Program.cs
@@ -4,86 +4,91 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
+using System.Windows.Forms;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
 
 
 namespace DLC_List_Generator_by_Frazzlee
-{
+{   
     class Program
     {
-        private static string[] foldernames;
 
         public static object exactnamearray { get; private set; }
 
+        [STAThread]
         static void Main(string[] args)
         {
-
-
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Changelog available on mod homepage");
-            Thread.Sleep(1900);
             
-
-
 
             //Variables
             string half1 = @"		<Item>dlcpacks:\";
             string half2 = @"\</Item>";
+            var config = new IniFile("Config.ini");
+            string gamePath = config.Read("gamePath");
+            string xmlLoc = "dlclist.xml";
 
-
-
-            //Setting up Console appearance
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.Clear();
-
-
-            // Reads the file as one string
-            //System.String directory = System.IO.File.ReadAllText(@"INFO.txt");
-
-            string directory1 = System.Reflection.Assembly.GetEntryAssembly().Location;
-
-            string directory = directory1.Remove(directory1.Length - 14);
-
-            Console.WriteLine(directory);
-
-
-            /*if (!Directory.Exists(directory))
-            {
-                Console.WriteLine("Specified path does not exist, change directory in INFO.txt");
-                Thread.Sleep(10000);
-                Environment.Exit(0);
+            if (!Directory.Exists(gamePath))
+            { 
+                Console.WriteLine(gamePath);
+                Console.WriteLine("Directory does not exist");
+                Console.ReadLine();
             }
-            else
-                Console.WriteLine("{0} is a valid directory", directory);
-                Thread.Sleep(2000);
-                Console.BackgroundColor = ConsoleColor.DarkBlue;
-                Console.Clear();*/
+            if(config.Read("getFrommods") == "False")
+            {
+                config.DeleteKey("getFrom");
+                config.Write("getFrom", gamePath + @"\update\x64\dlcpacks");
+            }
+            else if (config.Read("getFrommods") == "True")
+            {
+                config.DeleteKey("getFrom");
+                config.Write("getFrom", gamePath + @"\mods\update\x64\dlcpacks");
+            }
+            string directory = config.Read("getFrom");
+            config.DeleteKey("getFrom");
 
 
 
-
-            //Getting foldernames here and everything to do with folders and gettings names/paths
-            string[] allFolders = Directory.EnumerateDirectories(directory)
+                //Getting foldernames here and everything to do with folders and gettings names/paths
+                string[] allFolders = Directory.EnumerateDirectories(directory)
             .Select(d => new DirectoryInfo(d).Name).ToArray();
 
-            foreach (string folder in allFolders)
-                Console.WriteLine("{0}{1}{2}", half1, folder, half2);
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpBeach\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpBusiness\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpChristmas\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpValentines\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpBusiness2\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpHipster\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpIndependence\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpPilot\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\spUpgrade\\</Item>");
-                Console.WriteLine("		<Item>platform:\\dlcPacks\\mpLTS\\</Item>");             
-                System.IO.File.WriteAllLines("list.xml", allFolders);
-                //string[] dlcList = ;
 
-            Console.ReadLine();
+            File.WriteAllText(xmlLoc, @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<SMandatoryPacksData>
+	<Paths>
+		<Item>dlcpacks:\rdrcloud\</Item>
+		<Item>platform:\dlcPacks\mpBeach\</Item>
+		<Item>platform:\dlcPacks\mpBusiness\</Item>
+		<Item>platform:\dlcPacks\mpChristmas\</Item>
+		<Item>platform:\dlcPacks\mpValentines\</Item>
+		<Item>platform:\dlcPacks\mpBusiness2\</Item>
+		<Item>platform:\dlcPacks\mpHipster\</Item>
+		<Item>platform:\dlcPacks\mpIndependence\</Item>
+		<Item>platform:\dlcPacks\mpPilot\</Item>
+		<Item>platform:\dlcPacks\spUpgrade\</Item>
+		<Item>platform:\dlcPacks\mpLTS\</Item>
+");
+
+
+            foreach (string folder in allFolders)
+            {
+                File.AppendAllText(xmlLoc, half1 + folder + half2);
+                File.AppendAllText(xmlLoc, Environment.NewLine);
+            }
+
+            File.AppendAllText(xmlLoc, @"	</Paths>
+</ SMandatoryPacksData > ");
+
+            if (config.Read("copyToClipboard") == "True")
+            {
+                string clipboard = File.ReadAllText("dlclist.xml");
+                Clipboard.SetText(clipboard);
+            }
+
+
+
 
         }
     }
